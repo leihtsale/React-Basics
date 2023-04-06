5bf45615a7df4952be2371769042d60b80c83db0
elasthiel <elasthiel@gmail.com>
2023-04-07 06:24:19 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
+
+const App = () => {
+
+  return (
+    <Container>
+	</Container>
+  )
+}
+
+export default App;
