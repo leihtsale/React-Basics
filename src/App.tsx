f139014158d87f53c943f58f80993d28de82691e
elasthiel <elasthiel@gmail.com>
2023-04-09 09:29:36 +0800

    change single quotes to double

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import styled from 'styled-components';
-import { Container } from 'react-bootstrap';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
 
 const App = () => {
 
-  return (
-    <Container>
-	</Container>
-  )
+	return (
+		<Container>
+		</Container>
+	)
 }
 
 export default App;
